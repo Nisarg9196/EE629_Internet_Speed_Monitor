a366286dbb317a716aa662e2cd3e953d3772692c
Nisarg Parikh <60756156+Nisarg9196@users.noreply.github.com>
2020-05-17 16:52:46 -0400

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1,8 +1,10 @@
-import re 
+import os
+import re
 import subprocess
-from influxdb import InfluxDBClient
+import time
 
 response = subprocess.Popen('/usr/local/bin/speedtest-cli --simple', shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8')
+
 ping = re.findall('Ping:\s(.*?)\s', response, re.MULTILINE)
 download = re.findall('Download:\s(.*?)\s', response, re.MULTILINE)
 upload = re.findall('Upload:\s(.*?)\s', response, re.MULTILINE)
@@ -11,19 +13,11 @@ ping = ping[0].replace(',', '.')
 download = download[0].replace(',', '.')
 upload = upload[0].replace(',', '.')
 
-speed_data = [
-	{
-		"measurement" : "internet_speed",
-		"tags" : {
-			"host" : "RaspberryPiMyLife"
-		},
-		"fields" : {
-			"download" : float(download),
-			"upload" : float(upload),
-			"ping" : float(ping)
-		}
-	}
-]
+try:
+    f = open('/home/pi/speedtest/speedtest.csv', 'a+')
+    if os.stat('/home/pi/speedtest/speedtest.csv').st_size == 0:
+            f.write('Date,Time,Ping (ms),Download (Mbit/s),Upload (Mbit/s)\r\n')
+except:
+    pass
 
-client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'nisarg1996', 'internetspeed')
-client.write_points(speed_data)
+f.write('{},{},{},{},{}\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M'), ping, download, upload))
